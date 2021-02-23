827b533def8b35d96c29da96f7e241f061239741
josnavch <josnavch@gmail.com>
2021-02-23 02:42:07 +0000

    final changes JN

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // this is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
@@ -6,3 +7,28 @@ const sum = (a,b) => {
 // export the function to be used on other files 
 // (similar to the keyword `export` when using webpack)
 module.exports = { sum };
+
+*/
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+const fromDollarToYen = (dollar) => {
+    return dollar * oneEuroIs.JPY
+};
+fromEuroToDollar = (euro) => {
+    return euro * oneEuroIs.USD
+};
+fromYanToPound = (Yan) => {
+    return Yan * oneEuroIs.GBP
+
+}
+/*
+console.log(fromDollarToYen(5))
+console.log(fromEuroToDollar(5))
+console.log(fromYanToPound(5))
+*/
+
+module.exports = { fromDollarToYen, fromEuroToDollar, fromYanToPound };
--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-
+/*
 // import the function sum from the app.js file
 const { sum } = require('./app.js');
 
@@ -11,3 +11,30 @@ test('adds 14 + 9 to equal 23', () => {
     expect(total).toBe(23);
 });
 
+*/
+
+const { fromDollarToYen, fromEuroToDollar, fromYanToPound } = require('./exchange.js');
+
+test('Exchange $75 Dollars to Yen', () => {
+    //inside the test we call our sum function with 2 numbers
+    let total = fromDollarToYen(75);
+
+    // we expect the exchange $75 to Yen to be 9592.5
+    expect(total).toBe(9592.5);
+});
+
+test('Exchange $75 Dollars to EUROS', () => {
+    //inside the test we call our sum function with 2 numbers
+    let total = fromEuroToDollar(75);
+
+    // we expect the exchange $75 to EURO to be 90
+    expect(total).toBe(90);
+});
+
+test('Exchange $75 YAN to England Pound', () => {
+    //inside the test_exchange we call our exchange one number for a currency
+    let total = fromYanToPound(75);
+
+    // we expect the exchange $75 to England Pound to be 60
+    expect(total).toBe(60);
+});
